632dd305fd52caa47a36edaa7aa9a1955647de2f
santos ghimire <ghimiresantos85@gmail.com>
2025-05-27 17:46:51 +0545

    new connection string

---
+++ Services/MongoDbSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace MultipleDesk.Services
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+
+        public string RegistratrationCollectionName { get; set; } = "Registration";
+    }
+}
